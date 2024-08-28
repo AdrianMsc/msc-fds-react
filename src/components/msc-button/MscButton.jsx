4f2ce003bf6b2c222863c14cc22dcd81c6c98544
AdrianLona <jadrianlf@gmail.com>
2024-08-27 23:01:47 -0600

    feat msc button

---
+++ src/components/msc-button/MscButton.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const MscButton = ({
+  label = "Button",
+  variant = "solid",
+  size = "default",
+}) => {
+  const variantClasses = {
+    solid: "msc-btn msc-btn-blue-solid",
+    outline: "msc-btn msc-btn-blue-outline",
+    transparent: "msc-btn msc-btn-blue-transparent",
+  };
+
+  const variantSizes = {
+    default: "",
+    small: "msc-btn-sm",
+  };
+
+  const className = `${variantClasses[variant] || variantClasses.solid} ${
+    variantSizes[size] || variantSizes.default
+  }`;
+
+  return <button className={className}>{label}</button>;
+};
+
+export default MscButton;

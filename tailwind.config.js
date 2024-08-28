80481db7622f79678ae9fc4abbd4c64c8ab9c65e
AdrianLona <adrian.lona@mscsdirect.com>
2024-08-28 09:33:00 -0600

    feat(cb): tailwind colors and animations config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("./src/utilities/tailwind/colors");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+      keyframes: {
+        dotFlashing1: {
+          "0%, 100%": { opacity: "1" },
+          "50%": { opacity: "0" },
+        },
+        dotFlashing2: {
+          "0%": { opacity: "0.6" },
+          "50%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        dotFlashing3: {
+          "0%": { opacity: "0.3" },
+          "50%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        dotFlashing1: "dotFlashing1 2s infinite linear alternate",
+        dotFlashing2: "dotFlashing2 2s infinite linear alternate 0.4s",
+        dotFlashing3: "dotFlashing3 2s infinite linear alternate 0.6s",
+      },
+    },
   },
   plugins: [
     require("./src/utilities/tailwind/components/msc-buttons"),
